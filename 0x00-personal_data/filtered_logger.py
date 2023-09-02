8f0689ec1c28ec4df5c15218ebffd110e77e07e3
Ekarika Nsemeke <hamatthan@gmail.com>
2023-09-02 18:33:52 +0100

    Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -42,7 +42,7 @@ def get_logger() -> logging.Logger:
     logger = logging.getLogger('user_data')
     logger.setLevel(logging.INFO)
     logger.propagate = False
-    handler = logger.StreamHandler()
+    handler = logging.StreamHandler()
     handler.setFormatter(RedactingFormatter(PII_FIELDS))
     logger.addHandler(handler)
     return logger
