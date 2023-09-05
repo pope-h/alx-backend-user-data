5af40bca2c1543c9f3df104a552675ac9f629ce1
Ekarika Nsemeke <hamatthan@gmail.com>
2023-09-05 18:59:41 +0100

    Define which routes needs authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,8 +10,15 @@ class Auth():
     a class to manage the API authentication
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ returns False """
-        return False
+        """ checks path for auth """
+        check = path
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != '/':
+            check += '/'
+        if check in excluded_paths or path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ returns None """
