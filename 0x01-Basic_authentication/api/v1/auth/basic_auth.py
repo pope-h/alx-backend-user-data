0884f64779db40d656ef0e3dd3000f64b61cf4da
Ekarika Nsemeke <hamatthan@gmail.com>
2023-09-06 07:36:51 +0100

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 Basic Auth module
 """
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -20,3 +22,19 @@ class BasicAuth(Auth):
                 not authorization_header.startswith("Basic ")):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        returns the decoded value of a
+        Base64 string base64_authorization_header
+        """
+        b64AH = base64_authorization_header
+        if (b64AH is None or not isinstance(b64AH, str)):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(b64AH)
+        except (TypeError, binascii.Error):
+            return None
+        decoded_strings = decoded_bytes.decode('utf-8')
+        return decoded_strings
