49e1e6b6eb78eb99d2b3c5ed699126b48c176ff1
Ekarika Nsemeke <hamatthan@gmail.com>
2023-09-07 16:07:48 +0100

    Require auth with stars

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('unauthorized', methods=['GET'], strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """
     GET /api/v1/unauthorized
@@ -36,7 +36,7 @@ def unauthorized() -> str:
     return abort(401)
 
 
-@app_views.route('forbidden', methods=['GET'], strict_slashes=False)
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
     """
     GET /api/v1/forbidden
