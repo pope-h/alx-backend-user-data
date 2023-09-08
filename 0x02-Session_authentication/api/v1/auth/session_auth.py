9cd1816463b6f5a3d916b284148f4dcfd20845b1
Ekarika Nsemeke <hamatthan@gmail.com>
2023-09-08 19:24:54 +0100

    Logout

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -38,3 +38,19 @@ class SessionAuth(Auth):
             if session_cookie:
                 user_id = self.user_id_for_session_id(session_cookie)
                 return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """
+        deletes the user session / logout
+        """
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+
+        del self.user_id_by_session_id[session_id]
+        return True
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -3,9 +3,10 @@
 Session Authentication Views
 """
 from os import getenv
-from flask import jsonify, request
+from flask import jsonify, request, abort
 from api.v1.views import app_views
 from models.user import User
+from api.v1.app import auth
 
 
 @app_views.route('/auth_session/login', methods=['POST'],
@@ -34,3 +35,15 @@ def login():
             return response
         return jsonify({"error": "wrong password"}), 401
     return jsonify({"error": "no user found for this email"}), 404
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes='False')
+def destroy_session(self, request=None):
+    """ DELETE /auth_session/logout
+    Return:
+      - Response
+    """
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({}), 200
