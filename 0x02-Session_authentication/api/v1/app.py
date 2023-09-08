62c9e4df3894de7c9d12867640b1f9fffc469d91
Ekarika Nsemeke <hamatthan@gmail.com>
2023-09-08 08:19:50 +0100

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -60,10 +60,12 @@ def before_request():
         if auth.authorization_header(request) \
                 and auth.session_cookie(request) is None:
             abort(401)
-        request.current_user = auth.current_user(request)
         if not auth.current_user(request):
             abort(403)
-        
+        request.current_user = auth.current_user(request)
+        if auth.authorization_header(request) \
+                and auth.session_cookie(request) is None:
+            abort(401)
 
 
 if __name__ == "__main__":
