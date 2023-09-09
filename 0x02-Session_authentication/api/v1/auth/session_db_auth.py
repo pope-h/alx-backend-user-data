ed7b1e5acf84a9b3bfb25427d22b3dc0438005ad
Ekarika Nsemeke <hamatthan@gmail.com>
2023-09-09 18:32:13 +0100

    Sessions in database

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -9,23 +9,27 @@ class SessionDBAuth(SessionExpAuth):
     """ Session Authentication Class with Database """
     def create_session(self, user_id=None):
         """ Create a Session ID and store it in the database """
-        session_id = super().create_session((user_id))
+        session_id = super().create_session(user_id)
         if session_id is not None:
             user_session = UserSession(user_id=user_id, session_id=session_id)
             user_session.save()
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
-        """ Retrieve User ID for Session ID from the database """
+        """
+        Retrieve User ID for Session ID from the Database
+        """
         if session_id is None:
             return None
-            user_session = UserSession.search({'session_id': session_id})
-            if user_session:
-                return user_session[0].user_id
-            return None
+        user_session = UserSession.search({'session_id': session_id})
+        if user_session:
+            return user_session[0].user_id
+        return None
 
     def destroy_session(self, request=None):
-        """ Destroy UserSession based on Session ID from request cookie """
+        """
+        Destroy UserSession based on Session ID from request cookie
+        """
         if request is None:
             return False
         session_id = self.session_cookie(request)
