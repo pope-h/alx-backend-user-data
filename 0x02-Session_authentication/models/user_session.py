ced4650f711023016fa1dd7cd1fb63af08dc9caf
Ekarika Nsemeke <hamatthan@gmail.com>
2023-09-09 18:38:55 +0100

    Sessions in database

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -7,6 +7,7 @@ class UserSession(Base):
     """ session module """
 
     def __init__(self, *args: list, **kwargs: dict):
+        """ Initialize a UserSession instance """
         super().__init__(*args, **kwargs)
         self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')
