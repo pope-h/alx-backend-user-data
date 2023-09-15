b953fef3f88625a4e250762901e02524af6ad4ec
Ekarika Nsemeke <hamatthan@gmail.com>
2023-09-15 09:19:48 +0100

    create user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -12,8 +12,15 @@ class User(Base):
     SQLAlchemy model named User for a database table named users
     """
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
+        """
+        String rep.
+        """
+        return f"User: id={self.id}"
