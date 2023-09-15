11b64281524b93ab7b49d3ed04b936856cd811ec
Ekarika Nsemeke <hamatthan@gmail.com>
2023-09-15 09:31:53 +0100

    create user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -18,9 +18,3 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
-    def __repr__(self):
-        """
-        String rep.
-        """
-        return f"User: id={self.id}"
