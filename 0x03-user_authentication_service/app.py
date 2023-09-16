8cdaae01f46904fb3cb7880ab39d8fc0311182fe
Ekarika Nsemeke <hamatthan@gmail.com>
2023-09-16 23:43:56 +0100

    Basic Flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Flask app """
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashed=False)
+def home() -> str:
+    """home route view
+    """
+    payload = {"message": "Bienvenue"}
+    return jsonify(payload)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
