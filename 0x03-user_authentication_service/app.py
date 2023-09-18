9da554f613895226ebc2ff741771b4a0b2982769
Ekarika Nsemeke <hamatthan@gmail.com>
2023-09-18 04:44:46 +0100

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -31,6 +31,7 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login() -> str:
     """ User login app
     """
