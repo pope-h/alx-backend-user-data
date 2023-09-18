388d5fa04e4ff6460f02b1a857ad84c18284d7b6
Ekarika Nsemeke <hamatthan@gmail.com>
2023-09-18 05:44:49 +0100

    Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -54,7 +54,7 @@ def logout():
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id=session_id)
     if user:
-        AUTH.destroy_session(user_id)
+        AUTH.destroy_session(user.id)
         return redirect('/')
     abort(403)
 
